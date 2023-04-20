8f33185e2a676bd5155b000e2921bb87fa9ff81e
CollyLee <clh5160@gmail.com>
2023-04-19 20:59:14 -0400

    began get/post coding

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 
 // READS THE CURRENT JSON FILE, AND ADDS THE NEW CONTENT TO IT, AND WRITES TO A NEW JSON FILE
-const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
-};
+// const readAndAppend = (content, file) => {
+//     fs.readFile(file, 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             const parsedData = JSON.parse(data);
+//             parsedData.push(content);
+//             writeToFile(file, parsedData);
+//         }
+//     });
+// };
 
-const writeToFile = (destination, content) =>
-    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-        err ? console.error(err) : console.info(`\nData written to ${destination}`))
+// const writeToFile = (destination, content) =>
+//     fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+//         err ? console.error(err) : console.info(`\nData written to ${destination}`))
 
-const readFromFile = util.promisify(fs.readFile);
+// const readFromFile = util.promisify(fs.readFile);
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+// module.exports = { readFromFile, writeToFile, readAndAppend };
--- server.js
+++ server.js
@@ -1,30 +1,57 @@
-// GET /notes should return the notes.html file.
-// GET * should return the index.html file.
-
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-// Each note should have a randomized ID (uuid)
-
 const express = require('express');
-const api = require('./routes/index.js')
-const PORT = process.env.PORT || 3001;
+const PORT = /* process.env.PORT ||  */3002;
 const app = express();
 const uuid = require('./helpers/uuid');
 const fs = require('fs');
-const { readFromFile, writeToFile, readAndApend } = require('./helpers/fsUtils.js')
+// const { readFromFile, writeToFile, readAndApend } = require('./helpers/fsUtils.js')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const { noteTitle, noteText } = req.body
 
-if (noteTitle && noteText) {
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/notes.html')))
+
+app.post('/notes', (req, res) => {
+
+  console.info(`${req.method} request received to add a review`);
+
+  const { noteTitle, noteText } = req.body
+
+  if (noteTitle && noteText) {
     // Variable for the object we will save
-    const newReview = {
+    const newNote = {
       noteTitle,
       noteText,
       note_id: uuid(),
     };
-}
+  }
+
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      throw error;
+    } else {
+      const parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNote);
+
+      fs.writeFile('./db/db.json', JSON.stringify(parsedReviews),
+        (err) => err ? console.error(err) : console.info('Your note has been added'))
+    }
+  })
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
